a849b11653983b0a3e214aeb8c1b5261fbf21503
gonreg <vkaftancikov@gmail.com>
2023-04-22 15:29:01 +0300

    add cursor-pointer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.init()
 
+
 def draw_text(screen, text, size, x, y, color):
     font_name = pygame.font.match_font('arial')
     font = pygame.font.Font(font_name, size)
@@ -9,6 +10,7 @@ def draw_text(screen, text, size, x, y, color):
     text_rect = text_image.get_rect()
     text_rect.center = (x, y)
     screen.blit(text_image, text_rect)
+
 
 bounds = (360, 640)
 screen = pygame.display.set_mode(bounds)
@@ -27,13 +29,15 @@ while run:
     screen.fill('#FFFFFF')
 
     mousePos = pygame.mouse.get_pos()
-    print(mousePos)
+
     color = '#000000'
     if 360 > mousePos[0] > 360 - 90 and 640 > mousePos[1] > 640 - 90:
         color = '#353535'
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+    else:
+        pygame.mouse.set_cursor()
     pygame.draw.rect(screen, color, (360 - 90, 640 - 90, 90, 90))
     draw_text(screen, '=', 45, 360 - 45, 640 - 45, '#FFFFFF')
-
 
     pygame.draw.rect(screen, '#009515', (360 - 180, 640 - 90, 90, 90))
 
