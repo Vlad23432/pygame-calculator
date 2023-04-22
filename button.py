c7f7e99679cce5d208b1579eeddb37e7bbf920e4
nhoujuu10 <nhoujuu10@gmail.com>
2023-04-22 18:45:19 +0300

    .

---
+++ button.py
@@ -0,0 +1,38 @@
+import pygame
+class button:
+    color = None
+    colorHover = None
+    text = None
+    widht = None
+    height = None
+    bounds = None
+
+    def __init__(self, color, colorHover, text, widht, height, bounds, x ,y):
+        self.color = color
+        self.colorHover = colorHover
+        self.text = text
+        self.width = widht
+        self.height = height
+        self.bounds = bounds
+        self.x = x
+        self.y = y
+    def draw_text(self, screen):
+        font_name = pygame.font.match_font('arial')
+        font = pygame.font.Font(font_name, 45)
+        text_image = font.render(self.text, True, '#FFFFFF')
+        text_rect = text_image.get_rect()
+        text_rect.center = (self.x + self.width / 2, self.y + self.height / 2)
+        screen.blit(text_image, text_rect)
+
+    def draw(self, screen):
+        mousePos = pygame.mouse.get_pos()
+        if self.x + self.width > mousePos[0] > self.x and self.y + self.height > mousePos[1] > self.y:
+            color = self.colorHover
+        else:
+            color = self.color
+        pygame.draw.rect(screen, '#FFFFFF', (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(screen, color, (self.x + 2, self.y + 2, self.width - 4, self.height - 4))
+        self.draw_text(screen)
+
+    def click(self):
+        return
--- main.py
+++ main.py
@@ -1,45 +1,36 @@
 import pygame
-
+import button
 pygame.init()
-
-
-def draw_text(screen, text, size, x, y, color):
-    font_name = pygame.font.match_font('arial')
-    font = pygame.font.Font(font_name, size)
-    text_image = font.render(text, True, color)
-    text_rect = text_image.get_rect()
-    text_rect.center = (x, y)
-    screen.blit(text_image, text_rect)
-
-
 bounds = (360, 640)
+# buttonrovno = button.button('#000000', '#696969', '=', 90, 90, bounds, bounds[0] - 360, bounds[1] - 90 )
 screen = pygame.display.set_mode(bounds)
 
 pygame.display.set_caption('Calculator')
 clock = pygame.time.Clock()
-
+buttons = ['0', ',', '=', '1', '2', '3', '+','4','5','6','-','7','8','9','x','AC','+/-','%','/']
+buttonsObjects = []
+i = 1
+height = 90
+width = 360
+for btn in buttons:
+    if i % 4 == 0:
+        height += 90
+        width = 360
+    buttonsObjects.append(button.button('#000000', '#696969', btn, 180 if i == 1 else 90, 90, bounds, bounds[0] - width, bounds[1] - height))
+    width -= 180 if i == 1 else 90
+    i += 1
 run = True
 while run:
     clock.tick(60)
-
+    isClicked = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    screen.fill('#FFFFFF')
+    screen.fill('#ffffff')
 
-    mousePos = pygame.mouse.get_pos()
-
-    color = '#000000'
-    if 360 > mousePos[0] > 360 - 90 and 640 > mousePos[1] > 640 - 90:
-        color = '#353535'
-        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-    else:
-        pygame.mouse.set_cursor()
-    pygame.draw.rect(screen, color, (360 - 90, 640 - 90, 90, 90))
-    draw_text(screen, '=', 45, 360 - 45, 640 - 45, '#FFFFFF')
-
-    pygame.draw.rect(screen, '#009515', (360 - 180, 640 - 90, 90, 90))
+    for btn in buttonsObjects:
+        btn.draw(screen)
 
     pygame.display.flip()
 
